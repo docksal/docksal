3b81d9d432d6547d44cbc30384a00857e15751f2
Leonid Makarov <leonid.makarov@blinkreaction.com>
2015-04-09 12:59:52 -0400

    make install-dsh.sh compatible with boot2docker, fixes

--- install-dsh.sh
+++ install-dsh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Console colors
 red='\033[0;31m'
@@ -10,14 +10,14 @@ NC='\033[0m'
 DRUDE_REPO='https://github.com/blinkreaction/drude.git'
 DRUDE_REPO_RAW='https://raw.githubusercontent.com/blinkreaction/drude/master'
 
-# Detemine where we can install (/usb/local/bin or /bin)
-BIN='/usr/local/bin'
-touch "$BIN/dsh" 2> /dev/null || BIN="/bin"
-touch "$BIN/dsh" 2> /dev/null || { echo -e "${yellow}Warning: Not able to install dsh.${NC}"; }
 # Determine if we have sudo
 SUDO='sudo'
 if [[ -z $(which $SUDO) ]]; then SUDO=''; fi
+# Detemine where we can install (/usb/local/bin or /bin)
+BIN='/usr/local/bin'
+$SUDO touch "$BIN/dsh" 2> /dev/null || BIN="/bin"
+$SUDO touch "$BIN/dsh" 2> /dev/null || { echo -e "${yellow}Warning: Not able to install dsh.${NC}"; }
 # Install/update dsh tool wrapper
 echo -e "${green}Installing/updating dsh (Drude Shell) tool wrapper to $BIN/dsh${NC}"
-$SUDO curl -s "$DRUDE_REPO_RAW/scripts/dsh-wrapper.sh" > "$BIN/dsh"
+curl -s "$DRUDE_REPO_RAW/scripts/dsh-wrapper.sh" | $SUDO tee 1>/dev/null "$BIN/dsh"
 $SUDO chmod +x "$BIN/dsh"
