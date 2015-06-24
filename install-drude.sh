f9ca344bed87eb0a35ea6257520fd9e2973c55bf
Alexei Chekulaev <alexei.chekulaev@blinkreaction.com>
2015-06-24 15:52:28 -0400

    Improve install-drude script. Append testing possibility. Use DRUDE_TEST_ENVIRONMENT environment variable. Git remote update outpu suppressed. Git checkout forced to use server copy

--- install-drude.sh
+++ install-drude.sh
@@ -6,23 +6,28 @@ green='\033[0;32m'
 yellow='\033[1;33m'
 NC='\033[0m'
 
+# For testing
+BRANCH='master'
+if [ ! $DRUDE_TEST_ENVIRONMENT == "" ]; then
+	BRANCH=$DRUDE_TEST_ENVIRONMENT
+	echo -e "${red}[install-drude] testing mode: environment = \"${BRANCH}\"$NC"
+fi
+
 # Drude repo
-DRUDE_REPO='https://github.com/blinkreaction/drude.git'
-DRUDE_REPO_RAW='https://raw.githubusercontent.com/blinkreaction/drude/master'
+DRUDE_REPO="https://github.com/blinkreaction/drude.git"
+DRUDE_REPO_RAW="https://raw.githubusercontent.com/blinkreaction/drude/$BRANCH"
 
 # Yes/no confirmation dialog with an optional message
 # @param $1 confirmation message
 _confirm ()
 {
 	while true; do
-		read -p "$1Continue? [y/n]: " answer
+		read -p "$1 [y/n]: " answer
 		case $answer in
 			[Yy]|[Yy][Ee][Ss] )
-				echo "Working..."
 				break
 				;;
 			[Nn]|[Nn][Oo] )
-				echo 'Ok, whatever.'
 				exit 1
 				;;
 			* )
@@ -31,10 +36,21 @@ _confirm ()
 	done
 }
 
-echo "Updating dsh"
 # Install/update dsh tool wrapper
-curl -s "$DRUDE_REPO_RAW/install-dsh.sh" | bash
+curl -fsS "$DRUDE_REPO_RAW/install-dsh.sh" -o install-dsh.sh
+if [ $? -eq 0 ]; then
+	source install-dsh.sh
+	dsh_success=$?
+	rm install-dsh.sh
+	if [ ! $dsh_success -eq 0 ]; then
+		_confirm "Do you want to continue with drude update regardless?"
+	fi
+else
+	echo -e "${red}Could not get install-dsh script.${NC}"
+	_confirm "Do you want to continue with drude update regardless?"
+fi
 
+echo -e "${green}Installing Drude update...${NC}"
 # Check that git binary is available
 type git > /dev/null 2>&1 || { echo -e >&2 "${red}No git? Srsly? \n${yellow}Please install git then come back. Aborting...${NC}"; exit 1; }
 
@@ -43,28 +59,29 @@ if [[ -z $(git rev-parse --git-dir 2>/dev/null) ]]; then
 	# If there is no git repo - initialize one and commit everything before we proceed
 	echo -e "${yellow}No git repository! We'll create one to proceed with the install.${NC}"
 	echo -e "${green}Going to initialize a git repo and commit everything.${NC}"
-	_confirm "Initializing new git repo in $(pwd). "
+	_confirm "Do you want to initialize new git repo in $(pwd)?"
 	git init -q && git add -A && git commit -q -m 'Initial commit'
 fi
 
 # Make sure we are in the root of a git repository
 if [[ $(git rev-parse --git-dir) != '.git' ]]; then
 	GIT_ROOT=$(git rev-parse --show-toplevel)
-	echo -e "${yellow}This script must be run from the git repo root. Switching to $GIT_ROOT.${NC}"
+	echo "Switching to git repo root: $GIT_ROOT"
 	cd $GIT_ROOT
 fi
 
 # Check if the working tree and index are clean
 if [[ -n $(git status .docker docker-compose.yml -s) ]]; then
-	echo -e "${red}Your git working tree or index is not clean."
-	echo -e "It is recommended to commit any pending changes before proceeding with the update.${NC}"
-	_confirm "Going to overwrite changes to .docker/ and docker-compose.yml. "
+	echo -e "${yellow}Your have uncommitted changes in following files:${NC}"
+	git status .docker docker-compose.yml -s
+	echo -e "${yellow}Before proceeding with the update it is recommended to commit any pending changes in files above.$NC"
+	_confirm "Proceeding will overwrite your changes. Continue?"
 fi
 
 # Checking out the most recent Drude build
 git remote add drude $DRUDE_REPO
-git remote update drude
-git checkout drude/build .
+git remote update drude 1>/dev/null
+git checkout --theirs drude/devel .
 git remote rm drude
 
 if [[ -n $(git status .docker docker-compose.yml -s) ]]; then
