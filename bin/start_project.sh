e7f1151d228b398f9b6c7fb0a0eeaf0b95c1752b
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-09-24 21:20:20 -0700

    More testing and a bunch of refactoring

--- bin/start_project.sh
+++ bin/start_project.sh
@@ -1,48 +1,50 @@
 #!/bin/sh
 
-# TODO: this does not work as env variables are not passed/accessible here
+# TODO: this does not work as env variables are not passed/accessible here.
 if [[ $SUPERVISOR_DEBUG > 0 ]]; then
   echo "SUPERVISOR DEBUG"
   set -x
 fi
 
-set -x
+DOCKER_BIN="/usr/local/bin/docker"
+DC_BIN="/usr/local/bin/docker-compose"
 
-if [[ "$1" != "" && -f "$1" ]]; then
-  local DC_FILE="$1"
-  local DC_PROJECT="$(dirname $DC_FILE)"
-  local LOCK_FILE="$COMPOSE_PROJECT/.lock"
-  # Switchig to the project directory - docker-compose will pick up settings from there
+DC_FILE="$1"
+DC_PROJECT="$(dirname $DC_FILE)"
+LOCK_FILE="$COMPOSE_PROJECT/.lock"
+
+if [[ "$DC_FILE" != "" && -f $DC_FILE ]]; then
+  # Switchig to the project directory - docker-compose will pick up settings from there.
   cd $DC_PROJECT
 
-  if [[ ! -f "$LOCK_FILE" ]]; then
-    echo "Creating lockfile $LOCK_FILE"
-    touch "$LOCK_FILE"
+  # Using lockfile to avoid a race condition with multiple requests.
+  if [[ ! -f $LOCK_FILE ]]; then
+    touch $LOCK_FILE
 
-    # Figure out the default project network name
-    # TODO: refactor into something less hacky
-    local network="$(/usr/local/bin/docker-compose ps cli | awk 'NR==3 {print $1}' | sed 's/_cli_1//')_default"
-    # Docker Compose project name is an alphanumeric string derived from the directory name
-    #local network="$(echo $(basename $(dirname $COMPOSE_FILE)) | sed 's/[^[:alnum:]]*//g')_default"
-    /usr/local/bin/docker network connect "$network" vhost-proxy 2>&1
-    # Restart services if vhost-proxy was connected (first time) to the project network
-    # TODO: figure out how to avoid doing a restart
+    # Start containers.
+    echo "Starting containers for $DC_PROJECT..."
+    DC_OUTPUT=$($DC_BIN start 2>&1)
+    echo $DC_OUTPUT
+
+    # Figure out the default project network name from container names.
+    # Unfortunatelly relying on DC_OUTPUT from above is not reliable, so we'll use ps here.
+    #local network="$(echo $DC_OUTPUT | awk 'NR==3 {print $1}' | sed 's/\(.*\)_cli_1/\1/')_default"
+    network="$($DC_BIN ps | awk 'NR==3 {print $1}' | sed 's/\(.*\)_cli_1/\1/')_default"
+    # Connect proxy to the project network
+    $DOCKER_BIN network connect "$network" vhost-proxy 2>&1 >/dev/null
     if [[ $? == 0 ]]; then
-      echo "Connected vhost-proxy to \"${network}\" network."
-      # Restart to trigger docker-gen to regenerate nginx config
-      /usr/local/bin/docker-compose restart
-    else
-      # Start containers
-      /usr/local/bin/docker-compose start
+      echo "Connected proxy to network: ${network}."
     fi
+    # Trigger docker-gen with a dummy container to refresh nginx configuration.
+    echo "Trigerring docker-gen..."
+    $DOCKER_BIN run --rm busybox
 
-    echo "Removing lockfile $LOCK_FILE"
-    rm -rf "$LOCK_FILE"
+    rm -rf $LOCK_FILE
     exit 0
   else
-    echo "Project is locked with lockfile $LOCK_FILE"
+    echo "Project is locked ($LOCK_FILE)."
     exit 1
   fi
 fi
-echo "ERROR: Invalid or empty argument"
+echo "ERROR: Invalid or empty DC_FILE."
 exit 1
