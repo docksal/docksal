d01f98e33c1741b8802259a3db6c0546b61d2fc2
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2018-10-23 17:27:06 +0300

    Healthcheck added

---
+++ healthcheck.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# supervisor services are running
+if [[ -f /run/supervisord.pid ]]; then
+	[[ ! -f /run/nginx.pid ]] && exit 1
+	[[ ! -f /run/crond.pid ]] && exit 1
+fi
+
+exit 0
