80ffb73f67465cc886ee6725114c67901acaf895
Leonid Makarov <leonid.makarov@ffwagency.com>
2018-11-02 21:56:16 -0700

    Fix healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,6 +5,7 @@ if [[ -f /run/supervisord.pid ]]; then
 	supervisorctl status docker-gen | grep RUNNING >/dev/null || exit 1
 	supervisorctl status nginx | grep RUNNING >/dev/null || exit 1
 	supervisorctl status crond | grep RUNNING >/dev/null || exit 1
+	exit 0
 fi
 
 exit 1
