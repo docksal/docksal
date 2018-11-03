27cdd177077cbcc853ac7cb9a8e0e636045a39dd
Leonid Makarov <leonid.makarov@ffwagency.com>
2018-11-02 21:16:18 -0700

    Update healthcheck.sh
    Check all services via supervisorctl

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,9 +2,9 @@
 
 # supervisor services are running
 if [[ -f /run/supervisord.pid ]]; then
-	[[ ! -f /run/nginx.pid ]] && exit 1
-	[[ ! -f /run/crond.pid ]] && exit 1
 	supervisorctl status docker-gen | grep RUNNING >/dev/null || exit 1
+	supervisorctl status nginx | grep RUNNING >/dev/null || exit 1
+	supervisorctl status crond | grep RUNNING >/dev/null || exit 1
 fi
 
-exit 0
+exit 1
