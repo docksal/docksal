929adce1a862f6d9ef3b8d5c8f1a4a077a1097dc
Oleksii Chekulaiev <achekulaev@gmail.com>
2019-10-24 22:53:50 -0400

    Avoid warning in general.sh

--- tests/scripts/general.sh
+++ tests/scripts/general.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/docksal/drupal8.git ../drupal8
-
+git clone https://github.com/docksal/drupal8.git "$TRAVIS_BUILD_DIR/../drupal8"
 mkdir "$TRAVIS_BUILD_DIR/../test-init"
-mkdir "$TRAVIS_BUILD_DIR/../drupal8"
 cd "$TRAVIS_BUILD_DIR/../drupal8"
 bats "$TRAVIS_BUILD_DIR/tests/general.bats"
 
