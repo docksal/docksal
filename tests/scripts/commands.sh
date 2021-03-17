1fdfb336bb774c6e3e2e817badc2070ce7346e0f
Leonid Makarov <leonid.makarov@ffwagency.com>
2021-03-17 15:53:13 -0700

    Fixed typo in tests/scripts/commands.sh

--- tests/scripts/commands.sh
+++ tests/scripts/commands.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 mkdir "$GITHUB_WORKSPACE/../test-commands"
-cd mkdir "$GITHUB_WORKSPACE/../test-commands"
+cd "$GITHUB_WORKSPACE/../test-commands"
 bats "$GITHUB_WORKSPACE/tests/commands.bats"
