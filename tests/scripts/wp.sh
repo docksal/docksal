941c8ed777bd727425612f6dd72d0de5b220e7b3
Sean Dietrich <sean@kanopistudios.com>
2022-03-12 02:25:39 -0800

    Removed pull tests

--- tests/scripts/wp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-fin ssh-key add project_key
-
-mkdir "$GITHUB_WORKSPACE/../test-pull"
-cd "$GITHUB_WORKSPACE/../test-pull"
-bats "$GITHUB_WORKSPACE/tests/pull/wp.bats"
